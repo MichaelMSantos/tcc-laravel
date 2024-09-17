9a6165152e7ea33cee16f8dc9f1e480edff88edb
Michael M Santos <mmss240610@gmail.com>
2024-09-16 23:26:58 -0300

    modal structure and css

--- resources/views/dashboard/estoque/camisetas.blade.php
+++ resources/views/dashboard/estoque/camisetas.blade.php
@@ -90,11 +90,56 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Cadastrar nova camiseta</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    ...
+                    <label for="codigo">Codigo</label>
+                    <div class="input-content">
+                        <input type="text" name="codigo" id="codigo">
+                        <span> ou </span>
+                        <button type="button">Escanear</button>
+                    </div>
+                    <div class="input-content">
+                        <div class="input-group">
+                            <label for="modelo">Modelo</label>
+                            <input type="text" name="modelo" id="modelo">
+                        </div>
+                        <div class="option-group">
+                            <div class="input-group">
+                                <label for="cor">Cor</label>
+                                <select name="cor" id="cor">
+                                    <option value="">Branco</option>
+                                    <option value="">Preto</option>
+                                    <option value="">Amarelo</option>
+                                    <option value="">Vermelho</option>
+                                </select>
+                            </div>
+                            <div class="input-group">
+                                <label for="tamanho">Tamanho</label>
+                                <select name="tamanho" id="tamanho">
+                                    <option value="">P</option>
+                                    <option value="">M</option>
+                                    <option value="">G</option>
+                                    <option value="">GG</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-content">
+                        <div class="input-group">
+                            <label for="quantidade">Quantidade</label>
+                            <input type="text" name="quantidade" id="quantidade">
+                        </div>
+                        <div class="input-group">
+                            <label for="categoria">Categoria</label>
+                            <select name="categoria" id="categoria">
+                                <option value="">Adulto</option>
+                                <option value="">Jovem</option>
+                                <option value="">Criança</option>
+                            </select>
+                        </div>
+                    </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -15,6 +15,10 @@
     <link rel="stylesheet" href="/css/sidebar.css">
     <link rel="stylesheet" href="@yield('link')">
 
+    @if (Request::is('dashboard/estoque/camisetas'))
+        <link rel="stylesheet" href="/css/modal.css">
+    @endif
+
     {{-- Bootstrap Icon Link --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
