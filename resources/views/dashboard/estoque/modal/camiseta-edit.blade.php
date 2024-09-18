e87a43f053517adc2ef26f85f6d7600413a146ef
Michael M Santos <mmss240610@gmail.com>
2024-09-18 07:51:31 -0300

    correçao dos textos do modal de edicao

--- resources/views/dashboard/estoque/modal/camiseta-edit.blade.php
+++ resources/views/dashboard/estoque/modal/camiseta-edit.blade.php
@@ -6,7 +6,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Cadastrar nova camiseta</h1>
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Editar camiseta {{ $camiseta->codigo }}</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
@@ -61,8 +61,8 @@
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Understood</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                    <button type="submit" class="btn btn-primary">Confirmar</button>
                 </div>
             </div>
         </div>
