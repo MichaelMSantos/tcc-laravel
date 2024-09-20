7b01bdae18e46121b7170d2c73948da3bc7956c2
Caio <caioguedescaio52@gmail.com>
2024-09-20 11:26:54 -0300

    att tecido

--- app/Http/Controllers/TecidoController.php
+++ app/Http/Controllers/TecidoController.php
@@ -14,23 +14,44 @@ class TecidoController extends Controller
     }
 
     public function store(Request $request){
-        $tecido = new Tecido();
+        $tecidos = new Tecido();
 
-        $tecido->id = $request->id;
-        $tecido->medida = $request->medida;
-        $tecido->cor = $request->cor;
-        $tecido->quantidade = $request->quantidade;
-        $tecido->capacidade = $request->capacidade;
+        $tecidos->id = $request->id;
+        $tecidos->codigo =$request->codigo;
+        $tecidos->medida = $request->medida;
+        $tecidos->cor = $request->cor;
+        $tecidos->quantidade = $request->quantidade;
 
-        $tecido->save();
+        $tecidos->save();
 
         return back()->with('sucesso', 'Tecido registrado com sucesso');
     }
 
     public function edit($id) {
-        $tinta = Tecido::where('codigo', $id)->firstOrFail();
+        $tecido = Tecido::where('codigo', $id)->firstOrFail();
     
-        return view('dashboard.estoque.modal.tinta-edit', compact('tinta'));
+        return view('modal.estoque.tecido-edit', compact('tecido'));
 
     }
+    public function update(Request $request)
+    {
+
+        $tecido = Tecido::all();
+
+        Tecido::findOrFail($request->id)->update($request->all());
+
+
+        return back()->with('sucesso', 'Tecido atualizada com sucesso!');
+    }
+
+    public function destroy($id)
+    {
+        $tecido = Tecido::findOrFail($id);
+
+        $tecido->delete();
+
+        return redirect()->route('tecido.index')->with('sucesso', 'Produto excluido com sucesso');
+    }
 }
+
+
--- app/Models/Tecido.php
+++ app/Models/Tecido.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Tecido extends Model
 {
     use HasFactory;
+
+    use HasFactory;
+    protected $table = 'tecidos';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'codigo',
+        'medida',
+        'cor',
+        'quantidade',
+
+    ];
 }
+
