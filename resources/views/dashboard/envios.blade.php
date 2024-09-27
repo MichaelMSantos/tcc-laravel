ca7c321ae6f611a8d66bd0f691e28c3153dcb267
Michael M Santos <mmss240610@gmail.com>
2024-09-27 11:36:59 -0300

    bug envio

--- resources/views/dashboard/envios.blade.php
+++ resources/views/dashboard/envios.blade.php
@@ -25,24 +25,6 @@
                 <button id="novo" data-bs-toggle="modal" data-bs-target="#modal-envio">
                     Novo envio
                 </button>
-<nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);"
-    aria-label="breadcrumb">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="/dashboard">Dashboard</a></li>
-        <li class="breadcrumb-item active" aria-current="page">Enivo</li>
-    </ol>
-</nav>
-<div class="table-responsive">
-    <div class="table-header">
-        <div class="title">
-            Envios
-        </div>
-        <div class="actions">
-            <div class="search-box">
-                <span class="icon">
-                    <i class="bi bi-search"></i>
-                </span>
-                <input type="text" name="search" id="search" placeholder="Pesquisar fornecedor">
             </div>
         </div>
         <table class="table table-bordered">
